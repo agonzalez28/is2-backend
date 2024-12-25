290c8f7a5746d58b8fa0a7fcd146afeed018b340
agonzalez28 <anipau2001@fpuna.edu.py>
2024-12-25 19:01:08 -0300

    Se agrega endpoint para actualizar workspace y se ajusta el valor del estado

--- IS2/workspace/urls.py
+++ IS2/workspace/urls.py
@@ -6,5 +6,6 @@ from .views import crear_workspace, obtener_workspaces_por_usuario, eliminar_wor
 urlpatterns = [
     path('crear/', crear_workspace, name='crear_workspace'),
     path('workspaces/<str:cod_usuario>/', obtener_workspaces_por_usuario, name='obtener_workspaces_por_usuario'),
-     path('eliminar/<int:cod_espacio>/', eliminar_workspace, name='eliminar_workspace'),   
+    path('eliminar/<int:cod_espacio>/', eliminar_workspace, name='eliminar_workspace'),   
+    path('actualizar/<int:cod_espacio>/', actualizar_workspace, name='actualizar_workspace'),
 ]
--- IS2/workspace/views.py
+++ IS2/workspace/views.py
@@ -16,7 +16,7 @@ def crear_workspace(request):
             print("Datos recibidos:", data)
             nom_proyecto = data.get('nom_proyecto')
             descripcion = data.get('descripcion')
-            estado = data.get('estado', 'Publico')  # Valor por defecto: 'Publico'
+            estado = data.get('estado', 'Activo')  # Valor por defecto: 'Activo'
             cod_usuario = data.get('cod_usuario')
             cod_usuarios = data.get('cod_usuarios', [])  # Lista de códigos de usuarios a agregar
 
@@ -91,3 +91,40 @@ def eliminar_workspace(request, cod_espacio):
             return JsonResponse({'error': str(e)}, status=500)
 
     return JsonResponse({'error': 'Método no permitido'}, status=405)
+
+@csrf_exempt
+def actualizar_workspace(request, cod_espacio):
+    if request.method == "PUT":
+        try:
+            # Obtener los datos del cuerpo de la solicitud
+            data = json.loads(request.body)
+            print("Datos recibidos para actualizar:", data)
+
+            # Validar si se recibe el cod_espacio 
+            try:
+                workspace_obj = workspace.objects.get(cod_espacio=cod_espacio)
+            except workspace.DoesNotExist:
+                return JsonResponse({"error": "Workspace no encontrado"}, status=404)
+
+            # Validar que se ha enviado el nuevo nombre del proyecto
+            nom_proyecto = data.get("nom_proyecto")
+            if not nom_proyecto:
+                return JsonResponse({"error": "El nombre del proyecto es obligatorio"}, status=400)
+
+            # Actualizar campo de nombre del proyecto
+            workspace_obj.nom_proyecto = nom_proyecto
+            workspace_obj.save()
+
+            return JsonResponse({
+                "mensaje": "Workspace actualizado con exito",
+                "cod_espacio": workspace_obj.cod_espacio,
+                "nom_proyecto": workspace_obj.nom_proyecto
+            }, status=200)
+
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Error en el formato JSON"}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    return JsonResponse({"error": "Método no permitido"}, status=405)
+
